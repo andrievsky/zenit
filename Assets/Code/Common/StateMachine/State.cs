257aa6bf6a8c22a9ac9824537398e41810556ee1
nandrievsky <nandrievsky@gmail.com>
2018-06-18 00:36:08 +0200

    Add State Machine, Add Moq

---
+++ Assets/Code/Common/StateMachine/State.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Code.Common.StateMachine
+{
+    public interface IState
+    {
+        bool IsValidNextState(Type type);
+        IStateMachine<IState> StateMachine { get; }
+        void DidEnter(IState state);
+        void Update(int deltaTime);
+        void WillExit(IState state);
+    }
+    
+    public abstract class AbstractState : IState
+    {
+        private IStateMachine<IState> _machine;
+        
+        internal void Init(IStateMachine<IState> machine)
+        {
+            _machine = machine;
+        }
+
+        public bool IsValidNextState(Type type)
+        {
+            return true;
+        }
+
+        public IStateMachine<IState> StateMachine {
+            get { return _machine; }
+        }
+
+        public abstract void DidEnter(IState state);
+
+        public abstract void Update(int deltaTime);
+
+        public abstract void WillExit(IState state);
+    }
+}
---
+++ Assets/Code/Common/StateMachine/StateMachine.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Code.Common.StateMachine
+{
+    // Inspired by GKStateMachine https://developer.apple.com/documentation/gameplaykit/gkstatemachine
+    // Basic Sync implementation
+    public interface IStateMachine<T> where T : IState
+    {
+        void Init(T[] states);
+        T CurrentState { get; }
+        bool CanEnterState(Type type);
+        bool Enter(Type type);
+        void Update(int deltaTime);
+    }
+
+    public class SyncStateMachine : IStateMachine<IState>, IDisposable
+    {
+        private IState[] _states;
+
+        public IState CurrentState { get; private set; }
+
+        public void Init(IState[] states)
+        {
+            if (states == null)
+            {
+                throw new ArgumentException("states");
+            }
+            if (states.Length == 0)
+            {
+                throw new ArgumentException("At least one state should be available");
+            }
+            if (_states != null)
+            {
+                throw new Exception("State machime has been already init");
+            }
+            _states = states;
+            CurrentState = states[0];
+            CurrentState.DidEnter(null);
+        }
+
+        public bool CanEnterState(Type type)
+        {
+            return CurrentState.IsValidNextState(type);
+        }
+
+        public bool Enter(Type type)
+        {
+            if (!CanEnterState(type))
+            {
+                return false;
+            }
+            
+            IState next = null;
+            foreach (var state in _states)
+            {
+                if (state.GetType() != type)
+                {
+                    continue;
+                }
+                next = state;
+                break;
+            }
+
+            if (next == null)
+            {
+                return false;
+            }
+            
+            var prev = CurrentState;
+            CurrentState.WillExit(next);
+            CurrentState = next;
+            CurrentState.DidEnter(prev);
+            return true;
+        }
+
+        public void Update(int deltaTime)
+        {
+            CurrentState.Update(deltaTime);
+        }
+
+        public void Dispose()
+        {
+            if (CurrentState == null)
+            {
+                return;
+            }
+            CurrentState.WillExit(null);
+            CurrentState = null;
+            _states = null;
+        }
+    }
+}
